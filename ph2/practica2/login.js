71bb715de3bb01bf8fc9124abb04923379d1061e
hec17x <hec.medina.trabajo@gmail.com>
2016-04-08 15:51:48 +0200

    2

--- ph2/practica2/login.js
+++ ph2/practica2/login.js
@@ -8,12 +8,32 @@ function comprobarLogin(){
 
 function sendLogin()
 {
-   var url = "rest/viaje/?u="+cant;
+   var url = "rest/login/";
    var http_request = new createObject();
 
+   var user=document.getElementsByName('user')[0].value;
+   var pass=document.getElementsByName('pass')[0].value;
    
+   var http_request = new createObject();
 
-   http_request.open("GET", url, true); // Se crea petición GET a url, asíncrona ("true")
-   http_request.send(); // Se envía la petición
-   return http_request;
+   http_request.onreadystatechange = function()
+   {
+   	   alert(http_request.status+" "+http_request.readyState);
+   		if(http_request.readyState===4 && http_request.status==200)
+   		{
+      		//var aux = clean(http_request);
+      		//alert(http_request.responseText);
+      		var resp = JSON.parse(http_request.responseText);
+      		alert(resp.RESULTADO);
+
+
+      	}
+   };
+
+   http_request.open("POST", url, true); // Se crea petición GET a url, asíncrona ("true")
+  
+   http_request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+
+   http_request.send('login='+user+'&pwd='+pass); // Se envía la petición
+  
 }
